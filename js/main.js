fa6772f4c9b7b4c5855fd68e2cc3c7b5d4258e1a
Mstislav <stevemyz@gmail.com>
2014-04-29 22:07:10 +0700

    control done

--- js/main.js
+++ js/main.js
@@ -3,82 +3,47 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamewrap', {preload: preload,
 var playerShip;
 var sprite;
 var graphics;
-var isApply = false;
-var temp = [0, 0];
+var cursors;
 
 function preload() {
     game.load.image('background', 'img/bg.jpeg');
-    game.load.image('ship', 'img/rect.png');
+    game.load.image('ship', 'img/ship2.png');
 }
 
 function create() {
+    game.world.setBounds(0, 0, 1920, 1200);
     game.add.sprite(0, 0, 'background');
 
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.p2.world.applyDamping = false;
 
-    playerShip = new Ship();
+    game.time.advancedTiming = true;
 
     graphics = game.add.graphics(0, 0);
+    cursors = game.input.keyboard.createCursorKeys();
+
+    playerShip = new Ship();
+    game.camera.follow(playerShip.sprite);
+
 }
 
 function update() {
-    sprite.body.toWorldFrame(temp, [sprite.body.world.pxmi(50), sprite.body.world.pxmi(-50)]);
-    temp[0] = sprite.body.world.mpxi(temp[0]);
-    temp[1] = sprite.body.world.mpxi(temp[1]);
+    graphics.clear();
 
-    var alpha = sprite.body.rotation,
-        x = sprite.position.x + 50,
-        y = sprite.position.y - 50,
-        x2 = x * Math.cos(alpha) + y * Math.sin(alpha),
-        y2 = x * Math.sin(alpha) + y * Math.cos(alpha),
-        c = Math.sqrt(225),
-        dx = Math.cos(Math.PI / 4 + alpha) * c,
-        dy = Math.sin(Math.PI / 4 + alpha) * c;
-
-    if (!isApply) {
-        sprite.body.applyForce([dx, dy], temp[0], temp[1]);
-        // isApply = true;
+    if (cursors.left.isDown) {
+        playerShip.left();
     }
 
-    graphics.clear();
-    graphics.lineStyle(2, 0xFFFFFF, 1);
-    graphics.beginFill();
-    graphics.moveTo(temp[0], temp[1]);
-    graphics.lineTo(temp[0] - dx, temp[1] - dy);
-    graphics.endFill();
+    if (cursors.right.isDown) {
+        playerShip.right();
+    }
+
+    if (cursors.up.isDown) {
+        playerShip.thrust();
+    }
 }
 
 function render() {
     game.debug.spriteInfo(sprite, 32, 500);
-    game.debug.body(sprite);
+    game.debug.text(game.time.fps, 32, 400);
 }
-
-/*
-var renderer = PIXI.autoDetectRenderer(800, 600, false, false, true),
-    stage = new PIXI.Stage(0xFFFFFF);
-
-var shp;
-
-function pageOnLoad() {
-    var startTime = 0;
-    document.getElementById('gamewrap').appendChild(renderer.view);
-
-    shp = new Ship();
-    shp.show();
-
-    controls.init(shp);
-
-    requestAnimationFrame(animate);
-    function animate(time) {
-        var duration = time - startTime;
-
-        controls.checkPress();
-        shp.update(duration);
-
-        renderer.render(stage);
-
-        startTime = time;
-        requestAnimationFrame(animate);
-    }
-}*/
--- js/ship.js
+++ js/ship.js
@@ -1,56 +1,129 @@
 function Ship (param) {
-    this._color = 0xffffff;
-    this._size = 50;
-    this._weight = 100;
-    this._force = 1;
+    this.color = 0xffffff;
 
-    this._acceleration = false;
-    this._angle = 0;
-    this._speed = {
+    // 40x60
+    this.size = 50;
+    this.weight = 100;
+    this.forceThrust = 1000;
+    this.forceSide = 10;
+
+    this.speed = {
         x: 0,
         y: 0
     };
-    this._position = {
+
+    this.position = {
         x: 100,
         y: 100
     };
 
-    this._sprite = game.add.sprite(this._position.x, this._position.y, 'ship');
-    this._sprite.anchor.setTo(0.5, 0.5);
+    this.initPhysics();
 
-    sprite = this._sprite;
-    game.physics.p2.enable(this._sprite);
+    // debug
+    sprite = this.sprite;
+}
 
-    this._sprite.body.mass = 10;
-    // this._sprite.body.data.velocity[0] = -10;
+Ship.prototype.initPhysics = function () {
+    this.sprite = game.add.sprite(this.position.x, this.position.y, 'ship');
+    this.sprite.anchor.setTo(0.5, 0.5);
+
+    game.physics.p2.enable(this.sprite);
+    this.body = this.sprite.body;
+
+    this.body.mass = 10;
+}
+
+Ship.prototype.thrust = function () {
+    var point1 = this.worldPoint([0, 30]),
+        point2 = this.worldPoint([0, 40]);
+
+    this.body.thrust(this.forceThrust);
+
+    graphics.lineStyle(15, 0xFFFFFF, 1);
+    graphics.beginFill();
+    graphics.moveTo(point1[0], point1[1]);
+    graphics.lineTo(point2[0], point2[1]);
+    graphics.endFill();
+}
+
+Ship.prototype.left = function () {
+    var angle = this.body.rotation/* + Math.PI / 2*/,
+        point = this.worldPoint([20, -20]),
+        point2 = this.worldPoint([30, -20]),
+        pointBack = this.worldPoint([-20, 20]),
+        pointBack2 = this.worldPoint([-30, 20]),
+        force = [this.forceSide * Math.cos(angle), this.forceSide * Math.sin(angle)],
+        forceBack = [-force[0], -force[1]];
+
+    this.body.applyForce(force, point[0], point[1]);
+    this.body.applyForce(forceBack, pointBack[0], pointBack[1]);
+
+    graphics.lineStyle(5, 0xFFFFFF, 1);
+    graphics.beginFill();
+
+    graphics.moveTo(point[0], point[1]);
+    graphics.lineTo(point2[0], point2[1]);
+
+    graphics.moveTo(pointBack[0], pointBack[1]);
+    graphics.lineTo(pointBack2[0], pointBack2[1]);
+
+    graphics.endFill();
+}
+
+Ship.prototype.right = function () {
+    var angle = this.body.rotation + Math.PI,
+        point = this.worldPoint([-20, -20]),
+        point2 = this.worldPoint([-30, -20]),
+        pointBack = this.worldPoint([20, 20]),
+        pointBack2 = this.worldPoint([30, 20]),
+        force = [this.forceSide * Math.cos(angle), this.forceSide * Math.sin(angle)],
+        forceBack = [-force[0], -force[1]];
+
+    this.body.applyForce(force, point[0], point[1]);
+    this.body.applyForce(forceBack, pointBack[0], pointBack[1]);
+
+    graphics.lineStyle(5, 0xFFFFFF, 1);
+    graphics.beginFill();
+
+    graphics.moveTo(point[0], point[1]);
+    graphics.lineTo(point2[0], point2[1]);
+
+    graphics.moveTo(pointBack[0], pointBack[1]);
+    graphics.lineTo(pointBack2[0], pointBack2[1]);
+
+    graphics.endFill();
+}
+
+Ship.prototype.worldPoint = function (point) {
+    var wPoint = [];
+
+    this.body.toWorldFrame(wPoint, [this.body.world.pxmi(point[0]), this.body.world.pxmi(point[1])]);
+    wPoint[0] = this.body.world.mpxi(wPoint[0]);
+    wPoint[1] = this.body.world.mpxi(wPoint[1]);
+
+    return wPoint;
+}
+
+Ship.prototype.getX = function () {
+    return this.sprite.position.x;
+}
+
+Ship.prototype.getY = function () {
+    return this.sprite.position.y;
+}
+
+Ship.prototype.setX = function (val) {
+    this.sprite.position.x = val;
+    return this;
+}
+
+Ship.prototype.setY = function (val) {
+    this.sprite.position.y = val;
+    return this;
 }
 
 Ship.prototype.rotate = function (deg) {
-    this._sprite.body.rotation += deg;
+    this.sprite.body.rotation += deg;
 
     return this;
 }
-
-/*
-Ship.prototype.pullOn = function () {
-    this._acceleration = true;
-
-    return this;
-}
-
-Ship.prototype.update = function (time) {
-    if (this._acceleration) {
-        this._speed.x += this._force * Math.cos(this._angle) * time / (this._weight * 2);
-        this._speed.y += this._force * Math.sin(this._angle) * time / (this._weight * 2);
-    }
-
-    this._position.x += this._speed.x * time;
-    this._position.y -= this._speed.y * time;
-
-    this._graphics.position.x = this._position.x;
-    this._graphics.position.y = this._position.y;
-
-    this._acceleration = false;
-
-    return this;
-}*/
